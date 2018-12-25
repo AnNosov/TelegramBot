ace55ac00ba68e9c441adc7bb5d0fcd0396d180d
AnANosov <sir.baters@yandex.ru>
2018-12-25 12:39:07 +0300

    its better

--- TelegramBot.go
+++ TelegramBot.go
@@ -1,38 +1,66 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"gopkg.in/telegram-bot-api.v4"
 	"log"
 	"os"
 )
 
+type Config struct {
+	TelegramBotToken string
+}
+
 func main() {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	configuration := Config{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(configuration.TelegramBotToken)
 
-	//fmt.Println(configuration.TelegramBotToken)
-
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("783917145:AAHOcCXfORlcKaJ1leKttbs9apz1baIsZVA"))
+	bot, err := tgbotapi.NewBotAPI(configuration.TelegramBotToken)
 
 	if err != nil {
 		log.Panic(err)
 	}
-
-	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	// u - структура с конфигом для получения апдейтов
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
+	// используя конфиг u создаем канал в который будут прилетать новые сообщения
 	updates, err := bot.GetUpdatesChan(u)
 
-	if err != nil {
-		log.Panic(err)
-	}
-	// В канал updates будут приходить все новые сообщения.
+	// в канал updates прилетают структуры типа Update
+	// вычитываем их и обрабатываем
 	for update := range updates {
-		// Создав структуру - можно её отправить обратно боту
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		// универсальный ответ на любое сообщение
+		reply := "Не знаю что сказать"
+		if update.Message == nil {
+			continue
+		}
+
+		// логируем от кого какое сообщение пришло
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		// свитч на обработку комманд
+		// комманда - сообщение, начинающееся с "/"
+		switch update.Message.Command() {
+		case "start":
+			reply = "Привет. Я телеграм-бот"
+		case "hello":
+			reply = "What?"
+		}
+
+		// создаем ответное сообщение
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		// отправляем
 		bot.Send(msg)
 	}
 }
